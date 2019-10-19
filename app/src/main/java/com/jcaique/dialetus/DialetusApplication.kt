c3b6d68afed37e653f1c66b4a525fffa294311d3
Caique Oliveira <j.caique_fla@hotmail.com>
2019-10-19 18:30:34 -0300

    feat: dependecy injection (#11)
    * feat: dependency injection support
    
    * remove unecessary abstration

---
+++ app/src/main/java/com/jcaique/dialetus/DialetusApplication.kt
@@ -0,0 +1,25 @@
+package com.jcaique.dialetus
+
+import android.app.Application
+import com.jcaique.data.di.dataModule
+import org.kodein.di.Kodein
+import org.kodein.di.KodeinAware
+import org.kodein.di.conf.ConfigurableKodein
+import org.kodein.di.generic.bind
+import org.kodein.di.generic.provider
+
+class DialetusApplication : Application(), KodeinAware {
+
+    private val appModule = Kodein.Module(name = "application") {
+
+        bind() from provider {
+            this@DialetusApplication as Application
+        }
+    }
+
+    override val kodein = ConfigurableKodein(mutable = true).apply {
+        addImport(appModule)
+        addImport(dataModule)
+        addImport(presentation)
+    }
+}
---
+++ data/src/main/java/com/jcaique/data/Consts.kt
@@ -0,0 +1,5 @@
+package com.jcaique.data
+
+internal object Consts {
+    const val URL = "https://dialetus-service.herokuapp.com/"
+}
