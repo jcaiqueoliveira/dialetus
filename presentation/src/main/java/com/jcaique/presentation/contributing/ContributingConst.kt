e165a9d95eadaa169eb28f6c2ddbcad5c5d91b51
Caique Oliveira <j.caique_fla@hotmail.com>
2019-10-20 21:40:44 -0300

    fix visibility class

--- presentation/src/main/java/com/jcaique/presentation/contributing/ContributingConst.kt
+++ presentation/src/main/java/com/jcaique/presentation/contributing/ContributingConst.kt
@@ -1,6 +1,6 @@
 package com.jcaique.presentation.contributing
 
-object ContributingConst {
+internal object ContributingConst {
     const val ANDROID = "https://github.com/jcaiqueoliveira/dialetus"
     const val API = "https://github.com/mvfsillva/dialetus-service"
 }
