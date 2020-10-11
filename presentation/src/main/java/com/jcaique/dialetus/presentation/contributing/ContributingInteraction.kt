b280f10059cbb76e19f67c635f2d4a74ef0e0c86
Adriel Café <me@adriel.cafe>
2020-10-10 22:04:01 -0300

    fix: contributing web url

--- presentation/src/main/java/com/jcaique/dialetus/presentation/contributing/ContributingInteraction.kt
+++ presentation/src/main/java/com/jcaique/dialetus/presentation/contributing/ContributingInteraction.kt
@@ -2,5 +2,5 @@ package com.jcaique.dialetus.presentation.contributing
 
 internal sealed class ContributingInteraction(val url: String) {
     object OpenAndroid : ContributingInteraction(ContributingConst.ANDROID)
-    object OpenWeb : ContributingInteraction(ContributingConst.WEB)
+    object OpenWeb : ContributingInteraction(ContributingConst.API)
 }
