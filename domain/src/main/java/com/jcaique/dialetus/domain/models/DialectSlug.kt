c344c9dc531454f13669687040d3313db8b08fcf
Breno Cruz <brenocruzb@gmail.com>
2020-10-11 15:38:51 -0300

    fix: kotlinOptions xinline-classes

--- domain/src/main/java/com/jcaique/dialetus/domain/models/DialectSlug.kt
+++ domain/src/main/java/com/jcaique/dialetus/domain/models/DialectSlug.kt
@@ -1,5 +1,7 @@
 package com.jcaique.dialetus.domain.models
 
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import java.io.Serializable
 
+@OptIn(ExperimentalCoroutinesApi::class)
 inline class DialectSlug(val slug: String) : Serializable
