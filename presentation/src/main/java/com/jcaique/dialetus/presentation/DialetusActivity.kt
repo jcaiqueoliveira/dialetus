c9a0343385ddb6c8452606653c31022d953ad876
Breno Cruz <brenocruzb@gmail.com>
2020-10-14 18:42:54 -0300

    feat: toolbar (#57)
    Co-authored-by: Adriel Café <me@adriel.cafe>

--- presentation/src/main/java/com/jcaique/dialetus/presentation/DialetusActivity.kt
+++ presentation/src/main/java/com/jcaique/dialetus/presentation/DialetusActivity.kt
@@ -2,11 +2,26 @@ package com.jcaique.dialetus.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Icon
 import androidx.compose.foundation.Text
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Divider
+import androidx.compose.material.Scaffold
+import androidx.compose.material.TextField
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Search
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.unit.dp
 import androidx.ui.tooling.preview.Preview
 import com.jcaique.dialetus.presentation.ui.DialetusTheme
 
@@ -14,25 +29,84 @@ class DialetusActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            DialetusTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            App()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun App() {
+    DialetusTheme {
+        Scaffold(
+            topBar = {
+                AppTop()
+            },
+            bodyContent = {
+                AppContent()
+            },
+            bottomBar = {
+                AppBottom()
+            }
+        )
+    }
+}
+
+@Composable
+fun AppTop() {
+    Column {
+        val query = remember { mutableStateOf("") }
+
+        TopAppBar(
+            title = {
+                Text(text = "Baianes")
+            },
+            backgroundColor = Color.White,
+            elevation = 0.dp
+        )
+        TextField(
+            value = query.value,
+            placeholder = {
+                Text(
+                    text = "Pesquisar",
+                    color = Color.Gray
+                )
+            },
+            leadingIcon = {
+                Icon(
+                    asset = Icons.Outlined.Search,
+                    tint = Color.Gray
+                )
+            },
+            onValueChange = {
+                query.value = it
+            },
+            modifier = Modifier
+                .padding(horizontal = 10.dp)
+                .fillMaxWidth()
+                .clip(RoundedCornerShape(50)),
+            backgroundColor = Color.LightGray.copy(alpha = .2f),
+            activeColor = Color.Transparent
+        )
+        Divider(
+            color = Color.LightGray.copy(alpha = .4f),
+            thickness = 1.dp,
+            modifier = Modifier.padding(vertical = 16.dp)
+        )
+    }
+}
+
+@Composable
+fun AppContent() {
+    // TODO
+}
+
+@Composable
+fun AppBottom() {
+    // TODO
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    DialetusTheme {
-        Greeting("Android")
-    }
+    App()
 }
